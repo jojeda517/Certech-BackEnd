979d710ca43e68a30a0548d328acac993080eae9
José Ojeda <jojeda5171@uta.edu.ec>
2023-12-25 23:48:24 -0500

    CER-43 Endpoint para actualizar los datos de la firma
    Url: http://localhost:8000/api/firmaupdate/6/
    
    Nota: es un post

--- CertechBakend/api/urls.py
+++ CertechBakend/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import AdministradorView, FirmaView
+from .views import AdministradorView, FirmaView, FirmaUpdateView
 
 urlpatterns = [
     path('administrador/<str:usuario>/<str:clave>/',
          AdministradorView.as_view(), name='administrador_list'),
     path('firma/', FirmaView.as_view(), name='firma_list'),
-    path('firma/<str:id_firma>/', FirmaView.as_view(), name='firma_proceso')
+    path('firma/<str:id_firma>/', FirmaView.as_view(), name='firma_proceso'),
+    path('firmaupdate/<str:id_firma>/',
+         FirmaUpdateView.as_view(), name='firma_update')
 ]
